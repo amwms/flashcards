94ca1d277bc71cfa0fca6147a8aaca2093d35582
Anna Stawiska <ania.stawiska@gmail.com>
2023-02-04 19:54:21 +0100

    Added decks feature, corrected adding flashcards

--- index.py
+++ index.py
@@ -15,24 +15,25 @@ def index():
 
 @app.route('/card', methods=['GET'])
 def card():
-   return redirect('/card/0')
+   return redirect('/card_deck/default_deck/0')
 
-@app.route('/card/<id>', methods=['GET'])
-def card_id(id):
-   card, next_id = get_card(id)
+@app.route('/card_deck/<deck>/<id>', methods=['GET'])
+def card_deck(deck, id):
+   card, next_id = get_card(deck, id)
    title = 'Card ' + id
 
    return render_template('card.html', 
                            header_text=title,
                            flashcard_title=card['card_title'],
                            flashcard_text=card['card_text'],
-                           next_id=str(next_id))
+                           next_id=str(next_id),
+                           deck=deck)
 
-def get_card(id):
+def get_card(deck, id):
    cards = []
    with open('db/db.json', 'r') as open_file:
       json_object = json.load(open_file)
-      cards = json_object["card_data"]
+      cards = json_object[deck]
 
    id = int(id)
    next_id = id + 1
@@ -43,18 +44,24 @@ def get_card(id):
 @app.route('/add_card', methods=['GET', 'POST'])
 def add_card():
    if request.method == 'POST':    
-      # json_result = {
-      #    "card_title": request.form["card_title"],
-      #    # "card_label": request.form["card_text"],
-      #    "card_text": request.form["card_text"],
-      # }
+      json_result = {
+         "card_title": request.form["card_title"],
+         "card_text": request.form["card_text"],
+      }
 
-      # with open('db.json', 'r') as open_file:
-      #    json_object = json.load(open_file)
-      #    json_object["card_data"].append(json_result)
+      card_label = request.form["card_label"]
+
+      with open('db/db.json', 'r') as open_file:
+         json_object = json.load(open_file)
+
+         if card_label in json_object:
+            json_object[card_label].append(json_result)
+         else:
+            json_object[card_label] = []
+            json_object[card_label].append(json_result)
          
-      #    with open('db.json', 'w') as db_file:
-      #       json.dump(json_object, db_file, indent=4)
+         with open('db/db.json', 'w') as db_file:
+            json.dump(json_object, db_file, indent=4)
 
       return redirect('/add_card')
 
